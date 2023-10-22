ef5851a0bc3e751abcc2188d7a9ea240a268deee
ArinaSimka <141169470+ArinaSimka@users.noreply.github.com>
2023-10-22 22:38:06 +0300

    обновление gitignore

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿
+int GetNumber(string message)
+{
+    Console.Write($"Введите значение {message}:");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+string GetSt(string message)
+{
+    Console.Write($"Введите значение {message}:");
+    string n = Console.ReadLine();
+    return n;
+}
+
+string[] GetArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = GetSt("элемента массива");
+    }
+    return array;
+}
+
+void Print(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    { Console.Write($"   {arr[i]} "); }
+    Console.WriteLine();
+}
+
+string[] Select(string[] arr)
+{
+    int j = 0;
+    string[] arrayNew = new string[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+        string num = arr[i];
+        char[] array = num.ToCharArray();
+        if (array.Length < 4)
+        {
+            arrayNew[j] = arr[i];
+            j = j + 1;
+        }
+    }
+    return arrayNew;
+}
+
+int size = GetNumber("размерности массива");
+string[] array = GetArray(size);
+Print(array);
+Console.WriteLine("Значения, длина которых меньше, либо равна 3 символам:");
+string[] array2 = Select(array);
+Print(array2);
+
